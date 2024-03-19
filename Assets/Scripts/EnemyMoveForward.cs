d6d163cb42c087a1368d74e346260653b9048817
jaw071 <jaw071@student.usc.edu.au>
2024-03-19 15:20:48 +1000

    Formatted EnemyMoveForward.cs
    added comments, formatted code, changed variable name

--- Assets/Scripts/EnemyMoveForward.cs
+++ Assets/Scripts/EnemyMoveForward.cs
@@ -1,27 +1,24 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class EnemyMoveForward : MonoBehaviour {
-
+public class EnemyMoveForward : MonoBehaviour 
+{
     private float acceleration = 75f;
-
     private float initialVelocity = 2f;
-
-    private Rigidbody2D ourRigidbody;
+    private Rigidbody2D rb;
 
     // Use this for initialization
     void Start()
     {
-        ourRigidbody = GetComponent<Rigidbody2D>();
-
-        ourRigidbody.velocity = Vector2.down * initialVelocity;
+        rb = GetComponent<Rigidbody2D>();
+        // Set the initial velocity of the enemy
+        rb.velocity = Vector2.down * initialVelocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 ForceToAdd = Vector2.down * acceleration * Time.deltaTime;
-
-        ourRigidbody.AddForce(ForceToAdd);
+        // Apply force to the enemy based on acceleration and time
+        Vector2 forceToAdd = Vector2.down * acceleration * Time.deltaTime;
+        rb.AddForce(forceToAdd);
     }
 }
